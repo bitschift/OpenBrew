83c032525963af28ef97f0d7b980a074ebc4a4f8
Aravind G. Parasurama <aravindsasth@gmail.com>
2017-02-19 17:09:48 -0800

    Edited some more functions

--- src/avr/main.c
+++ src/avr/main.c
@@ -8,7 +8,7 @@ int main(void) {
 
 	uart_init(BAUD_RATE);	// Init UART at 38400 baud
 	CPU_PRESCALE(0x01);		// run at 8MHz.
-	timer_init();			// Will set up PWM for Peltier and hardware clock
+	timer_init();			// Will set up PWM for heating, and hardware clock
 	adc_init();				// Will set up ADC for reading the thermistor
 	sei();					// Start interrupts.
 
@@ -42,16 +42,7 @@ int main(void) {
 	}
 }
 
-void run_temperature(char temp) {
-
-}
-
-void run_stir(char intensity) {
-
-}
-
-void phase(char temp, char time){
-	uint16_t timer = time * 1000;					// Converts user-provided seconds to milliseconds
+void run_temperature(char temp, int holdtime) {
 	uint8_t ch = 0x00;								// Indicates PF0 as read port
 	uint8_t tempin = (uint8_t)adc_read(ch)>>1; 		// Divide by 2
 
@@ -72,21 +63,16 @@ void phase(char temp, char time){
 		uart_putchar('\n');
 
 		find_duty(tempin, temp);
-		if(tempin > temps) {
-			uart_putstring("dc\n");					// Send cooling flag to Android
-			OCR1A = duty;							// Set duty cycle to heating direction
-			OCR1B = 0;
-		}
 		else if(tempin < temps) {
 			uart_putstring("dh\n");					// Send heating flag to Android
 			OCR1A = 0;
-			OCR1B = duty;							// Set duty cycle to cooling direction
+			OCR1B = duty;							// Set duty cycle to heating direction
 		}
 	}
 
 	//holding temp, similar loop to above, but will hold a temperature for a set time
 	count_ms = 0;
-	while(count_ms < timer){
+	while(count_ms < holdtime){
 		uart_putstring("sh\n");				// Send hold flag to Android
 		hold = 1;							// find_duty() now knows that we're holding
 
@@ -97,14 +83,7 @@ void phase(char temp, char time){
 		uart_putchar('\n');
 
 		find_duty(tempin, temps);
-		if(tempin > temps){
-			uart_putstring("dc\n");
-			OCR1A = duty;
-			OCR1B = 0;
-			uint16_t x = count_ms;
-			while(count_ms < x + 150){};
-		}
-		else if(tempin < temps){
+		if(tempin < temps){
 			uart_putstring("dh\n");
 			OCR1A = 0;
 			OCR1B = duty;
@@ -121,74 +100,11 @@ void phase(char temp, char time){
 	hold = 0;
 }
 
-void pcr_init(void){
-	char pcr_set[15];
-	pcr_set[0] = '0';
-	pcr_set[13] = '\n';
-	pcr_set[14] = '\0';
-	// while loop will check with user to verify that input are correct.
-	while(1){
-		//Getting user settings from bluetooth (12 values: 6 temp, 6 time)
-		uint8_t i;
-		for(i=1; i<13;i++){
-			pcr_set[i]= uart_getchar();
-		}
+void run_stir(char intensity) {
 
-		//Confirming user settings
-		uart_putchar(1);
-		uart_putstring(pcr_set);
-
-		if(uart_getchar() != '0'){
-			//********** PCR Steps Time & Temperature
-
-			// Initialization step:
-			init_step_time = pcr_set[1];
-			init_step_temp = pcr_set[2];
-
-			//Denaturation step:
-			Den_step_time = pcr_set[3];
-			Den_step_temp = pcr_set[4];
-
-			// Annealing step:
-			ann_step_time = pcr_set[5];
-			ann_step_temp = pcr_set[6];
-
-			//Extension/elongation step:
-			ext_step_time = pcr_set[7];
-			ext_step_temp = pcr_set[8];
-
-			//Final elongation step:
-			f_elon_step_time = pcr_set[9];
-			f_elon_step_temp = pcr_set[10];
-
-			//Final hold:
-			f_hold_step_time = pcr_set[11];
-			f_hold_step_temp = pcr_set[12];
-
-			uart_putstring("START!\n");
-
-			break;
-		}
-	}
 }
 
-void run_pcr (void){
-	char x;
-	for(x = 0; x < 30; x ++){
-		uart_putchar('y'); // send Android current cycle
-		uart_putchar(x+1);
-		uart_putchar('\n');
-
-		uart_putstring("p1\n"); // send Android current phase
-		phase(Den_step_temp, Den_step_time); // initiate denaturing
-		uart_putstring("p2\n");
-		phase(ann_step_temp, ann_step_time); // initiate annealing
-		uart_putstring("p3\n");
-		phase(ext_step_temp, ext_step_time); // initiate extension
-	}
-}
-
-/* This function uses an equation to find the duty cycle that the PWM-powered Peltier junction
+/* This function uses an equation to find the duty cycle that the PWM heating element
    should operate at at any given time. */
 void find_duty(char tempin, char temp){
 	/*if (hold == 0) {
